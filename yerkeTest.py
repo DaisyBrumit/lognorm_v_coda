0121bcd13a1c6840c2fa38a6fa90dda60523ce29
DaisyBrumit <dfrybrum@uncc.edu>
2023-03-07 16:25:56 -0500

    including metadata

--- yerkeTest.py
+++ yerkeTest.py
@@ -4,13 +4,15 @@ import pandas as pd
 import numpy as np
 from skbio.stats import composition
 from sklearn.model_selection import train_test_split
-from sklearn.ensenble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # Establish sets for testing
 pathList = ['Daisy_16S/Jones_ForwardReads_DADA2.txt', 'Daisy_16S/Vangay_ForwardReads_DADA2.txt','Daisy_16S/Zeller_ForwardReads_DADA2.txt']
 
 # load in set, generate zero sums
-df = pd.read_table(pathList[0])
+#df = pd.read_table(pathList[0])
+df = pd.read_table('/Users/dfrybrum/Documents/FodorLab/gemelli/Jones/Jones_ForwardReads_DADA2.txt')
+
 df_sub = df.iloc[:,0:50]
 count_list = []
 
@@ -39,7 +41,12 @@ logOut = table.divide(rowSums)
 logOut = np.log10(logOut*avg + 1)
 
 # import metadata for RF
-
+meta = pd.read_csv('/Users/dfrybrum/Documents/FodorLab/gemelli/Jones/jones_meta.csv')
+meta_sub = meta[meta.iloc[:,0].isin(table.index)]
+meta_sub.set_index(keys=meta_sub['Run'], drop=True)
+## sanity check
+if len(meta_sub.index) != len(table.index): print("Metadata and table data do not include same sample count!")
+else: print("Metadata/data sample check passed")
 
 # Random Forest
 
