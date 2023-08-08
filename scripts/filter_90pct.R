a24cd1084d4de2b76abe7776864d67c50ebc7db7
DaisyBrumit <dfrybrum@uncc.edu>
2023-08-08 13:59:21 -0400

    filter seqs in less than 10% of samples

---
+++ scripts/filter_90pct.R
@@ -0,0 +1,28 @@
+# FILTER OUT COLUMNS WITH MORE THAN 90% ZEROS 
+# IN KEEPING WITH SAME METHOD USED BY AARON YERKE
+# INPUT: DADA2 TABLE
+# OUTPUT: FILTERED DADA2 TABLE
+
+
+library(tidyverse)
+
+study.list <- c('Zeller', 'Jones', 'Noguera-Julian', 'Vangay')
+genPath.in <- '~/beta_diversity_testing/'
+genPath.out <- '~/lognorm_v_coda/'
+
+for (study in study.list) {
+  
+  in_path <- paste0(genPath.in,study,'/',study,'_ForwardReads_DADA2.txt') # set path for original DADA2
+  setwd(paste0(genPath.out,study))
+  
+  raw.table <- read.table(in_path, header=TRUE, row.names = 1)
+  
+  # FILTER SEQUENCES WITH 90% THRESHOLD 
+  zero.row <- as.data.frame(rowSums(raw.table == 0))
+  zero.row <- zero.row / ncol(raw.table)
+  zero.row <- zero.row %>% filter(`rowSums(raw.table == 0)` < 0.9)
+  rows.to.keep <- rownames(zero.row)
+  
+  rowfiltered.table <- raw.table %>% filter(row.names(.) %in% rows.to.keep) %>% rownames_to_column(var='rownames')
+  write_tsv(rowfiltered.table, 'rowFiltered90_DADA2.txt')
+}
