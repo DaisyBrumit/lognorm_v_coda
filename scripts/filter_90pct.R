f15a2e086d08b1d857872991458c1b0d4284fbf5
DaisyBrumit <dfrybrum@uncc.edu>
2023-08-09 11:24:09 -0400

    add efficient method

--- scripts/filter_90pct.R
+++ scripts/filter_90pct.R
@@ -18,10 +18,15 @@ for (study in study.list) {
   raw.table <- read.table(in_path, header=TRUE, row.names = 1)
   
   # FILTER SEQUENCES WITH 90% THRESHOLD 
+  # LONG WAY TO CHECK SANITY
   zero.row <- as.data.frame(rowSums(raw.table == 0))
   zero.row <- zero.row / ncol(raw.table)
   zero.row <- zero.row %>% filter(`rowSums(raw.table == 0)` < 0.9)
   rows.to.keep <- rownames(zero.row)
+  
+  # A MUCH MORE EFFICIENT METHOD, WILL BE TRANSPOSED
+  # df1 <- as.data.frame(t(raw.table))
+  # df2 <- df1[,sapply(df1, function(x) mean(x == 0) < 0.9)]
   
   rowfiltered.table <- raw.table %>% filter(row.names(.) %in% rows.to.keep) %>% rownames_to_column(var='rownames')
   write_tsv(rowfiltered.table, 'rowFiltered90_DADA2.txt')
